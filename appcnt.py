a58ba4a971268d489368124cba84a2e515e06c87
kasumiiii <chianti24x@gmail.com>
2018-12-13 18:07:24 +0900

    ファイル分割

--- appcnt.py
+++ appcnt.py
@@ -1,5 +1,8 @@
-from server import Judge 
+#from server import Judge 
 
-class Val:
-  name = Judge().judge_data()
-  print(name, '(appcnt)')
+class Webapp:
+  #print('hello_Webapp')
+  def val(self, input_list):
+    #input_list = Input().judge_data()
+    print(input_list)
+
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 # server.py
 from flask import Flask, render_template, request
-from cerberus import Validator
 import re
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+from appcnt import Webapp
 
 """
 #バリデーション定義
@@ -32,7 +33,7 @@ schema = {
 }
 """
 # バリデータを作成
-v = Validator(schema)
+#v = Validator(schema)
 
 app = Flask(__name__)
 
@@ -42,26 +43,38 @@ Base = declarative_base()
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/welcome', methods=['POST'])
-def welcome():
 
-                name = request.form['name_form']
-                add = request.form['add_form']
-                addnum = request.form['addnum_form']
-                mail = request.form['mail_form']
-                tel = request.form['tel_form']
+class Judge:
+  @app.route('/welcome', methods=['POST'])
+ # def printtest(self):
+ #   print('hello')
 
+#class Judge:
+  def judge_data(self):
+    print('hello judge_data') 
+    input_list = [
+      request.form['name_form'],
+      request.form['add_form'],
+      request.form['addnum_form'],
+      request.form['mail_form'],
+      request.form['tel_form'],
+    ]
+
+    Webapp().val(input_list)
+
+#@app.route('/welcome', methods=['POST'])
+  def welcome():
+                
 		#バリデーション
-                v.validate(name, add, addnum, mail, tel)
+                #v.validate(name, add, addnum, mail, tel)
 
 		#ins = "INSERT INTO users (name, address, addnum, mail, tel) VALUES (%s, %s, %s, %s, %s)"
                 #data = [( name, add, addnum, mail, tel )]
                 #for d in data:
                 #    engine.execute(ins,d)	
-
-                pprint(v.errors)
+                name = 'hoge'
                 return render_template('welcome.html', name=name ) 
 
 if __name__ == '__main__':
